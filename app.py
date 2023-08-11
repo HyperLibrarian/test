daf7914259108e6f4750f4d676b1a7c9c8eedc38
HyperLibrarian <413067884@qq.com>
2023-08-11 13:49:19 +0800

    first commit

--- app.py
+++ app.py
@@ -7,62 +7,94 @@ import os
 import multiprocessing as mp
 import time
 
+camera_g = None
+status = 1
+
 class VideoCamera():
     def __init__(self):
         # 通过opencv获取实时视频流
         self.video = cv2.VideoCapture(0) 
         print(os.getpid())
-        print(0, self.video.isOpened())
+        print(0, self.video.isOpened()) 
+        global status
+        if self.video.isOpened() == False :
+            status = 0
+            print(00)
     
     def __del__(self):
         print(1)
         self.video.release()
-        time.sleep(10)    
+        #time.sleep(3)
+          
 
     def get_frame(self):
-        print(2)
-        print(self.video.isOpened())
+        # print(2)
+        # print(self.video.isOpened())
         success, image = self.video.read()
-        print(3)
+        # print(3)
         # 因为opencv读取的图片并非jpeg格式，因此要用motion JPEG模式需要先将图片转码成jpg格式图片
         if image is not None:
-            print(8)
+            # print(8)
             ret, jpeg = cv2.imencode('.jpg', image)
             return jpeg.tobytes()
         else:
-            print(9)
+            # print(9)
             return None
-
-
+        
+    def release(self):
+        self.video.release()
 
 def gen(camera):
+
     result = True
     while result:
-        print(5)
+        # print(5)
         frame = camera.get_frame()
         # cv2.waitKey(24)
         # 使用generator函数输出视频流， 每次请求输出的content类型是image/jpeg
         if frame:
-            print(6)
+            # print(6)
             yield (b'--frame\r\n'
                     b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
         else:
-            print(7)
+            # print(7)
             yield (b'--frame\r\n'
                     b'Content-Type: image/jpeg\r\n\r\n' + b'\r\n\r\n')
 
+
 app = Flask(__name__)
 
-@app.route('/')  # 主页
+@app.route('/api/video/video_feed', methods=['GET'])  # 主页
 def index():
     # jinja2模板，具体格式保存在index.html文件中
     return render_template('index.html')
 
-@app.route('/api/video/video_feed', methods=['GET'])  # 返回视频流响应
+@app.route('/api/video/video_feed/source', methods=['GET'])  # 返回视频流响应
 def video_feed():
-    camera = VideoCamera()
-    return Response(gen(camera),
-                    mimetype='multipart/x-mixed-replace; boundary=frame') 
+    print(123)
+    global status
+    global camera_g
+    camera_g = None
+
+    #if status == 0:
+    if 1 == 0:
+        response = {
+        "code":0,
+        "msg":"相机刚才进行拍摄或暂停",
+        "data": status
+        }
+        status = 1
+        return jsonify(response)
+
+
+    else:
+        camera = VideoCamera()
+        camera_g = camera
+        print(222, camera_g)
+        return Response(gen(camera),
+                        mimetype='multipart/x-mixed-replace; boundary=frame') 
+
+
   
 
 @app.route('/api/video/stop_stream', methods=['GET'])   # 设备关闭
@@ -71,7 +103,17 @@ def stop_stream():
     #VideoCamera().stop_stream
     #lib0 = ctypes.CDLL('./lib/libdemo4.so')  # 加载共享库
     #lib0.USBReboot()
-    result = False
+    #video0 = cv2.VideoCapture(1) 
+    result = True
+    global camera_g
+    global status
+    print(111, camera_g)
+    if camera_g:
+        camera_g.release()
+        camera_g = None
+        status = 0
+        result = False
+    
     response = {
         "code":0,
         "msg":"相机暂停",
@@ -109,15 +151,22 @@ def capture():   # 拍摄
     str = './Img/Color_0000.png&./Img/Depth_0000.png'  # 字符串序列
     color_png,  depth_png= str.split('&', 1)
 
+    global camera_g
+    global status
+    if camera_g:
+        camera_g.release()
+        camera_g = None
+        status = 0
     video1 = cv2.VideoCapture(0) 
     _, frame1 = video1.read()
     cv2.imwrite(color_png, frame1)
     video1.release()
+    video1 = None
 
-    video2 = cv2.VideoCapture(1) 
-    _, frame2 = video2.read()
-    cv2.imwrite(depth_png, frame2)
-    video2.release()
+    #video2 = cv2.VideoCapture(1) 
+    #_, frame2 = video2.read()
+    #cv2.imwrite(depth_png, frame2)
+    #video2.release()
 
     response = {
         "code":0,
@@ -132,6 +181,7 @@ def segment():   # 测距
     data = request.get_data()
     data = json.loads(data)
     depth_path = data['data'][0]['depth_png']# 本地图片路径
+    print(depth_path)
     #lib1 = ctypes.CDLL('./lib/libdemo1.so')  # 加载共享库
     #lib1.CalCylinder()
 
@@ -151,4 +201,4 @@ def segment():   # 测距
 
 
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0', port = 5001, debug = True)  
+    app.run(host = '0.0.0.0', port = 5001, debug = False)  
--- testcam.py
+++ testcam.py
@@ -1,7 +1,36 @@
-import cv2
+import numpy as np
+import sys
+import cv2 as cv
 
-cams_test = 5
-for i in range(0, cams_test):
-    cap = cv2.VideoCapture(i)
-    test, frame = cap.read()
-    print("i : "+str(i)+" /// result: "+str(test))
+
+def main():
+    # Open Orbbec depth sensor
+    orbbec_cap = cv.VideoCapture(0, cv.CAP_OBSENSOR)
+    if orbbec_cap.isOpened() == False:
+        sys.exit("Fail to open camera.")
+
+    while True:
+        # Grab data from the camera
+        if orbbec_cap.grab():
+            # RGB data
+            ret_bgr, bgr_image = orbbec_cap.retrieve(None, cv.CAP_OBSENSOR_BGR_IMAGE)
+            if ret_bgr:
+                cv.imshow("BGR", bgr_image)
+
+            # depth data
+            ret_depth, depth_map = orbbec_cap.retrieve(None, cv.CAP_OBSENSOR_DEPTH_MAP)
+            if ret_depth:
+                color_depth_map = cv.normalize(depth_map, None, 0, 255, cv.NORM_MINMAX, cv.CV_8UC1)
+                color_depth_map = cv.applyColorMap(color_depth_map, cv.COLORMAP_JET)
+                cv.imshow("DEPTH", color_depth_map)
+        else:
+            print("Fail to grab data from the camera.")
+
+        if cv.pollKey() >= 0:
+            break
+
+    orbbec_cap.release()
+
+
+if __name__ == '__main__':
+    main()
